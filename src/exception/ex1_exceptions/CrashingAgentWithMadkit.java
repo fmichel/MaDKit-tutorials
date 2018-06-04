7a30903c21fc94149fef55b75d51bea5cc64ba5d
Amandine Paillard <amandine.paillard@hotmail.fr>
2018-06-04 14:16:02 +0200

    ex1_exceptions : adding society constants to help user to understand the next exercices

--- src/exception/ex1_exceptions/CrashingAgentWithMadkit.java
+++ src/exception/ex1_exceptions/CrashingAgentWithMadkit.java
@@ -1,15 +1,20 @@
 package exception.ex1_exceptions;
 
+import static  exception.ex1_exceptions.Society.COMMUNITY;
+import static  exception.ex1_exceptions.Society.GROUP;
+import static  exception.ex1_exceptions.Society.ROLE_CRASH;
+
 import madkit.kernel.Agent;
 import madkit.kernel.Message;
 
+//TODO doc and see / explain Kernel exceptions
 public class CrashingAgentWithMadkit  extends Agent {
 	
 	public static void main(String[] argss) {
 		Agent launcherAgent = new Agent();
 		CrashingAgentWithMadkit agent = new CrashingAgentWithMadkit();
 		launcherAgent.launchAgent(agent);
-		agent.sendMessage("aCommunity", "aGroup", "aRole", new Message());
+		agent.sendMessage(COMMUNITY, GROUP, ROLE_CRASH, new Message());
 	}
 
 }
---
+++ src/exception/ex1_exceptions/Society.java
@@ -0,0 +1,8 @@
+package exception.ex1_exceptions;
+
+public class Society {
+	public static final String COMMUNITY = "exception";
+    public static final String GROUP = "ex01";
+    public static final String ROLE_CRASH = "crashingAgent";
+    public static final String ROLE_LAUNCH = "launchingAgent";
+}
