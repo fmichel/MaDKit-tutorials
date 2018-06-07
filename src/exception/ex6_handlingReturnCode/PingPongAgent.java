1678d0d70bd7a6af24f47eb19ca0ef85bb2a8bd0
Amandine Paillard <amandine.paillard@hotmail.fr>
2018-06-07 14:35:00 +0200

    ex6 created : real uses of return codes

--- src/exception/ex6_handlingReturnCode/PingPongAgent.java
+++
@@ -1,8 +0,0 @@
-package exception.ex6_handlingReturnCode;
-
-/**TODO faire un exemple développé en testant les codes de retour de quelques fonctions ;
- * Montrer que l'on peut ainsi mieux "manager" / sécuriser son programme. Cf. tuto-exceptions.md
- */
-public class PingPongAgent {
-
-}
