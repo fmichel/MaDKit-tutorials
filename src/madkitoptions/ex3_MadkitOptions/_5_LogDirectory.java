7886fbb86ab99a88d55ec2ecd65ac4089d190ae4
Amandine Paillard <amandine.paillard@hotmail.fr>
2018-07-12 15:45:07 +0200

    options tutorial done !

--- src/madkitoptions/ex3_MadkitOptions/_5_LogDirectory.java
+++ src/madkitoptions/ex3_MadkitOptions/_5_LogDirectory.java
@@ -1,6 +1,7 @@
 package madkitoptions.ex3_MadkitOptions;
 
 import madkit.kernel.Madkit;
+import madkitoptions.TutorialAgent;
 
 /**
  * This option allows MaDKit users to specify
@@ -16,7 +17,11 @@ public class _5_LogDirectory {
 
 	public static void main(String[] args) {
 		
-		/* Here the wanted directory is : toto */
-		new Madkit(Madkit.BooleanOption.createLogFiles.toString(), Madkit.Option.logDirectory.toString(), "toto");
+		/**
+		 *  Here the wanted directory is : myLogs.
+		 *  We are launching a TutorialAgent to generate logs.
+		 *  Do not forget also to allow the creation of log files.
+		 */
+		new Madkit(Madkit.Option.logDirectory.toString(), "myLogs", Madkit.BooleanOption.createLogFiles.toString(),Madkit.Option.launchAgents.toString(), TutorialAgent.class.getName());
 	}
 }
